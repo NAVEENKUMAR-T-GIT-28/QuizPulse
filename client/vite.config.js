1d948053aac3c6c220d562e25084804b329a7d74
NAVEENKUMAR-T-GIT-28 <naveenkumarsnr28@gmail.com>
2026-05-09 08:31:38 +0530

    fix vite config process lint issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const target = env.VITE_SERVER_URL || 'http://localhost:5000'
-  
+
   const allowedHosts = ['localhost']
+
   if (env.VITE_ALLOWED_HOST) {
     allowedHosts.push(env.VITE_ALLOWED_HOST)
   }
